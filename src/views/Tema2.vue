580e557ebf544e4310a34e2257eae53eef3184a7
payepesch <maechavarriaor@gmail.com>
2023-04-25 14:58:02 -0500

    Tema 2 Carga

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,53 +5,26 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Cómo iniciar las pruebas o testing en un videojuego
+      h1 Diseño Curricular
 
     .row.justify-content-center.align-items-center
       .col-6.col-lg-2.mb-3
         figure
           img(src='@/assets/curso/temas/tema2/img1.png', alt="alt")      
       .col-lg-10
-        p En teoría, cualquiera puede aprender a probar un juego, incluso si no está capacitado para ser un ingeniero de control de calidad. Sin embargo, tener éxito y eficacia en las pruebas es otra cuestión, y los proyectos a gran escala generalmente necesitan evaluadores calificados para tener éxito. Por lo tanto, puede considerar contratar a estos profesionales o delegar las pruebas a una empresa con amplia experiencia en el campo.
-        p Cualquiera que sea la opción que elija, el proceso de prueba será más o menos el mismo. Entendemos que las docenas de tipos de pruebas que enumeramos son únicas y requieren #[i software], metodología y conocimientos diferentes, pero las etapas de las pruebas suelen ser uniformes como se observa a continuación:
+        p La noción de diseño puede emplearse para nombrar a la delineación de algo (un proyecto, una obra, etc.). Curricular, por su parte, es aquello vinculado a un currículo: un programa de estudios.
 
+        p La idea de diseño curricular, por lo tanto, hace referencia a un proceso que permite organizar y desarrollar un plan educativo. A través de esta estructuración se busca satisfacer las necesidades formativas de los estudiantes.
+
+        p El diseño curricular se plasma en un documento que presenta la estructura del plan de educación, detallando las características y proyectando los alcances de la formación. En este documento se mencionan los objetivos del currículo, las competencias que se buscan desarrollar, los resultados que se persiguen y la certificación que se brinda.
+
+    separador
+    #t_2_1.titulo-segundo.color-acento-contenido
+      h2 2.1 Objetivos de la planificación
+    
     .row.justify-content-center.my-5
-      .col-lg-8
-        PasosA.color-acento-contenido.my-5(tipo="n")
-          .row
-            .col-md-8.mb-4.mb-md-0
-              h3 Planificación
-              p Tienes que caminar antes de correr y las pruebas no son diferentes. En lugar de probar cuando surge la necesidad, puede hacer que el proceso sea más eficiente creando un plan integral con una lista de casos de prueba y cronología.
-            .col-md-4.mt-4
-              figure
-                img(src='@/assets/curso/temas/tema2/img2.png', alt='Texto que describa la imagen')
 
-          .row
-            .col-md-4.mb-4.mb-md-0
-              figure
-                img(src='@/assets/curso/temas/tema2/img3.png', alt='Texto que describa la imagen')
-            .col-md-8
-              h3 Pruebas
-              p No hay mucho que decir sobre esta etapa. Una prueba puede durar entre unos minutos y varios días. No es raro que se realicen varias pruebas simultáneamente.
-              
-
-          .row
-            .col-md-8.mb-4.mb-md-0
-              h3 Informes de errores y correcciones
-              p Los ingenieros de control de calidad encuentran muchos tipos de errores en las pruebas de juegos y es su trabajo documentarlos todos. Los datos de la prueba y los errores/problemas identificados se empaquetan en un informe de errores y se envían a los desarrolladores para que los corrijan en una compilación futura.
-          
-            .col-md-4.mt-4
-              figure
-                img(src='@/assets/curso/temas/tema2/img4.png', alt='Texto que describa la imagen')
-
-          .row
-            .col-md-8.mb-4.mb-md-0
-              h3 Informes de errores y correcciones
-              p Los ingenieros de control de calidad encuentran muchos tipos de errores en las pruebas de juegos y es su trabajo documentarlos todos. Los datos de la prueba y los errores/problemas identificados se empaquetan en un informe de errores y se envían a los desarrolladores para que los corrijan en una compilación futura.
-          
-            .col-md-4.mt-4
-              figure
-                img(src='@/assets/curso/temas/tema2/img5.png', alt='Texto que describa la imagen')
+      p Gracias al diseño curricular, es posible aseverar que la formación es pertinente de acuerdo al grado de formación. Los docentes encuentran en él una guía para llevar adelante la labor pedagógica.
 
     .row
       .col-lg-7.mb-4
@@ -60,9 +33,7 @@
             .list__item__icon
               img(src='@/assets/curso/temas/tema1/icon1.svg', alt='')
           .list__txt
-            h3.mb-0.tv.margin-icon Evaluación y pruebas
-
-      p.mb-5 Este apartado tiene como objetivo comentar las pruebas realizadas en el juego desarrollado. Las pruebas son un elemento fundamental en un desarrollo #[i software] y deben realizarse siempre para evitar fallos no deseados. Existen los siguientes tipos de pruebas: 
+            h3.mb-0.tv.margin-icon Puede decirse que el diseño curricular define:
 
     .tarjeta--container.row.mb-5
       .col-md.tarjeta.color-acento-contenido.p-4
@@ -71,9 +42,8 @@
             figure
               img(src='@/assets/curso/temas/tema2/img6.png', alt='Texto que describa la imagen')
           
-        h4.text-center Pruebas unitarias
-        p.text-center Estas pruebas tienen como objetivo verificar la funcionalidad y estructura de cada componente individual de código realizado. Son pruebas a nivel de métodos codificados.
-
+        h4.text-center Qué
+        p.text-center 
 
       .col-md.tarjeta.color-secundario.p-4
         .row.justify-content-center.mb-4
@@ -81,9 +51,8 @@
             figure
               img(src='@/assets/curso/temas/tema2/img7.png', alt='Texto que describa la imagen')
           
-        h4.text-center.texto-negro Pruebas de integración
-        p.text-center.texto-negro Su objetivo es comprobar el correcto ensamblaje entre los distintos componentes del código, cubren la funcionalidad establecida y se ajustan, en mayor medida, a los requisitos funcionales.
-
+        h4.text-center.texto-negro Cómo
+        p.text-center.texto-negro 
 
       .col-md.tarjeta.color-acento-botones.p-4
         .row.justify-content-center.mb-4
@@ -91,8 +60,8 @@
             figure
               img(src='@/assets/curso/temas/tema2/img8.png', alt='Texto que describa la imagen')
           
-        h4.text-center Pruebas de sistema
-        p.text-center Están destinadas a probar el sistema como un todo y se centran en los requisitos no funcionales como por ejemplo la gestión de recursos, compatibilidad, usabilidad. Suelen estar enfocadas a la gestión de los recursos como el procesador o la memoria. 
+        h4.text-center Cuándo
+        p.text-center 
 
       .col-md.tarjeta.color-primario.p-4
         .row.justify-content-center.mb-4
@@ -100,9 +69,50 @@
             figure
               img(src='@/assets/curso/temas/tema2/img9.png', alt='Texto que describa la imagen')
           
-        h4.text-center.texto-negro Pruebas de aceptación
-        p.text-center.texto-negro Se usan para validar que el sistema cumple con el funcionamiento esperado y permitir al usuario del sistema que determine su aceptación sobre la funcionalidad y el rendimiento. 
+        h4.text-center.texto-negro Donde
+        p.text-center.texto-negro 
 
+    separador
+    #t_2_2.titulo-segundo.color-acento-contenido
+      h2 2.2 Cómo se realiza el diseño curricular
+
+    p Para poder llevar adelante un diseño curricular se hace necesario tener claro que hay que llevar adelante una serie de tareas como son las siguientes:
+
+    .tarjeta--morada.p-4.mb-5.mt-5
+      .row
+        .col-lg-5.mb-4
+          figure
+            img(src='@/assets/curso/temas/tema3/img11.svg', alt='Texto que describa la imagen').img-11    
+
+        .col-lg-7
+          ol.lista-ol--cuadro
+            li 
+              .lista-ol--cuadro__vineta
+                span a
+              | El diagnóstico de problemas y necesidades en cuanto a lo que es el contexto y la situación existente. En concreto, esta labor se compone de tres etapas fundamentales: el estudio de lo que es el marco teórico, la exploración de situaciones reales y la determinación de problemas y necesidades.
+            
+            li 
+              .lista-ol--cuadro__vineta
+                span b
+              | La modelación del currículum, en la que, por ejemplo, se determinan los contenidos necesarios para alcanzar los objetivos deseados.
+
+            li 
+              .lista-ol--cuadro__vineta
+                span c
+              | La estructuración curricular, donde se elige, por ejemplo, el orden en el que se va a desarrollar el diseño curricular.
+
+            li 
+              .lista-ol--cuadro__vineta
+                span d
+              | La organización para la puesta en práctica, es decir, el establecimiento de medidas para garantizar lo que es la puesta en práctica del proyecto curricular.
+            
+            li 
+              .lista-ol--cuadro__vineta
+                span 
+              | El diseño de la evaluación curricular, que debe tener como punto de partida lo que son objetivos de tipo terminal.
+
+    .row.mt-5
+      p El diseño curricular, en definitiva, es el procedimiento que posibilita la planificación general de las actividades académicas. El currículo, una vez definido, luego se adapta a las condiciones particulares de cada establecimiento educativo, siempre respetando los principios establecidos en el diseño. De esta manera se pretende garantizar que cada uno de los estudiantes pueda desarrollar su potencial, formarse y concluir el proceso educativo preparado para integrarse a un nivel superior o al universo laboral.
 
 
 </template>
